c6e36d8ac80a34b26cfbbe46fec482c0fe0cba10
Alexander Slesarenko <avslesarenko@gmail.com>
2022-05-02 13:03:51 +0200

    added referenced to npm modules

--- core/js/src/main/scala/org/ergoplatform/crypto.scala
+++ core/js/src/main/scala/org/ergoplatform/crypto.scala
@@ -4,6 +4,7 @@ import scala.annotation.nowarn
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
+/** See package info at https://www.npmjs.com/package/elliptic */
 @JSImport("elliptic", "ec")
 @js.native
 @nowarn
@@ -15,6 +16,7 @@ object EC {
   val curve = new EC("secp256k1")
 }
 
+/** See package info at https://www.npmjs.com/package/blakejs */
 @JSImport("blakejs", JSImport.Default)
 @js.native
 object blake extends js.Object {
@@ -32,31 +34,35 @@ object blake extends js.Object {
   def blake2bFinal(ctx: Context): js.Array[Short] = js.native
 }
 
+/** See package info at https://www.npmjs.com/package/hash.js and also tests and type defs
+  * at https://github.com/indutny/hash.js
+  */
 @JSImport("hash.js", JSImport.Default)
 @js.native
 object Hash extends js.Object {
   def sha256(): Sha256 = js.native
   val utils: Utils = js.native
+
+  @js.native
+  trait Utils extends js.Object {
+    @nowarn
+    def toArray(msg: String, enc: String): js.Array[Short] = js.native
+    @nowarn
+    def toHex(msg: js.Array[Short]): String = js.native
+  }
+
+  @js.native
+  trait MessageDigest[T] extends js.Object {
+    val blockSize: Int = js.native
+    val outSize: Int = js.native
+  }
+
+  @js.native
+  trait Sha256 extends js.Object with MessageDigest[Sha256] {
+    @nowarn
+    def update(msg: String, enc: String): Sha256 = js.native
+    @nowarn
+    def digest(enc: String): String = js.native
+  }
 }
 
-@js.native
-trait Utils extends js.Object {
-  @nowarn
-  def toArray(msg: String, enc: String): js.Array[Short] = js.native
-  @nowarn
-  def toHex(msg: js.Array[Short]): String = js.native
-}
-
-@js.native
-trait MessageDigest[T] extends js.Object {
-  val blockSize: Int = js.native
-  val outSize: Int = js.native
-}
-
-@js.native
-trait Sha256 extends js.Object with MessageDigest[Sha256] {
-  @nowarn
-  def update(msg: String, enc: String): Sha256 = js.native
-  @nowarn
-  def digest(enc: String): String = js.native
-}
