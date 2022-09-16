a07debfc3ed66bfdda4b4485880acace5fe3f6b9
Alexander Slesarenko <avslesarenko@gmail.com>
2022-09-16 19:04:49 +0200

    js files for tests

---
+++ tests/js/bindings.spec.js
@@ -0,0 +1,9 @@
+const {
+  bouncyCastle,
+} = require("../../core/js/target/scala-2.13/core-test-fastopt/main.js");
+
+describe("Smoke tests for generated JS", () => {
+  it("Should have method", () => {
+    expect(bouncyCastle.interpolate).not.toBeUndefined();
+  });
+});
